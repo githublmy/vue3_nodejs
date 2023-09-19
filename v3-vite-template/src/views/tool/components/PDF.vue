92951e7e1e1d2e52a23e2e00610e984972f1ff18
455886774@qq.com <lu123456>
2023-09-19 10:15:52 +0800

    添加PDF预览

--- v3-vite-template/src/views/tool/components/PDF.vue
+++ v3-vite-template/src/views/tool/components/PDF.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    PDF
+  <div style="height: 100%">
+    <VuePdfApp style="height: calc(100vh - 80px)" :pdf="pd"></VuePdfApp>
   </div>
 </template>
 
 <script lang="ts" setup>
-         
+import VuePdfApp from "vue3-pdf-app";
+import "vue3-pdf-app/dist/icons/main.css";
+import pd from "@/assets/document/test2.pdf";
 </script>
 <style lang="scss" scoped>
-         
+:deep(.pdf-vue3-canvas-container) {
+  height: calc(100vh - 80px);
+}
 </style>
