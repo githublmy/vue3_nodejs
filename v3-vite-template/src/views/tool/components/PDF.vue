824c78da6f8763519abbe820df155402da9d10a5
455886774@qq.com <lu123456>
2023-09-20 09:33:42 +0800

    修改配置

--- v3-vite-template/src/views/tool/components/PDF.vue
+++ v3-vite-template/src/views/tool/components/PDF.vue
@@ -1,14 +1,14 @@
 <template>
   <div style="height: 100%">
+    <!-- 中文包 -->
     <link rel="resource" type="application/l10n" href="/src/assets/document/viewer.properties">
-    <VuePdfApp :pdf="pd"></VuePdfApp>
+    <VuePdfApp pdf=""></VuePdfApp>
   </div>
 </template>
 
 <script lang="ts" setup>
 import VuePdfApp from "vue3-pdf-app";
 import "vue3-pdf-app/dist/icons/main.css";
-import pd from "@/assets/document/test2.pdf";
 
 </script>
 <style lang="scss" scoped>
