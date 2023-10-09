676b260683345311b640c094b32ef3b2f4014d88
455886774@qq.com <lu123456>
2023-10-09 15:12:03 +0800

    电子签名

--- v3-vite-template/src/components/Sign/index.vue
+++ v3-vite-template/src/components/Sign/index.vue
@@ -8,6 +8,7 @@
           :height="signConfig.height"
           @mouseleave="mouseleave"
           @mousedown="mousedown"
+          @mouseup="mouseup"
           @mousemove="mousemove"
         >
           你的浏览器不支持 canvas，请升级你的浏览器。
@@ -20,11 +21,22 @@
           label-width="80px"
           :inline="false"
         >
+          <el-form-item label="画布宽度" prop="width">
+            <el-input-number v-model="signConfig.width" :min="0" size="small" />
+          </el-form-item>
+          <el-form-item label="画布高度" prop="height">
+            <el-input-number
+              v-model="signConfig.height"
+              :min="0"
+              size="small"
+            />
+          </el-form-item>
           <el-form-item label="背景颜色" prop="fillStyle">
             <el-color-picker
               v-model="signConfig.fillStyle"
               size="large"
               show-alpha
+              @change="changeBgColor"
             />
           </el-form-item>
           <el-form-item label="画笔颜色" prop="color">
@@ -32,6 +44,7 @@
               v-model="signConfig.color"
               size="large"
               show-alpha
+              @change="setColor"
             />
           </el-form-item>
           <el-form-item label="画笔粗细" prop="lineWidth">
@@ -39,43 +52,154 @@
               v-model="signConfig.lineWidth"
               :min="0"
               step-strictly
+              @change="setLineWidth"
+              size="small"
             />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" size="small" @click="">重置</el-button>
+            <el-button type="primary" size="small" @click="reset"
+              >重置</el-button
+            >
           </el-form-item>
         </el-form>
       </el-col>
     </el-row>
     <el-row>
       <el-col>
-        <el-button type="primary" size="small" @click="">上一步</el-button>
-        <el-button type="primary" size="small" @click="">清空</el-button>
-        <el-button type="primary" size="small" @click="">保存图片</el-button>
+        <el-button type="primary" size="small" @click="preStep"
+          >上一步</el-button
+        >
+        <el-button type="primary" size="small" @click="clearCanvas"
+          >清空</el-button
+        >
+        <el-button type="primary" size="small" @click="save"
+          >保存图片</el-button
+        >
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script lang="ts" setup>
-const signConfig = reactive({
+const signConfig = ref({
   width: 800,
   height: 300,
-  fillStyle: "#FFFFFF", //画布背景颜色
+  fillStyle: "#fff", //画布背景颜色
   color: "#000", //画笔颜色
-  lineWidth: 3, //画笔粗细
+  lineWidth: 4, //画笔粗细
 });
+
+// 是否按下状态
+let isDown = false,
+  canvas: HTMLCanvasElement, //canvas的dom
+  ctx: CanvasRenderingContext2D, //canvas实例
+  imgDataArr: ImageData[] = []; ////存储线条数据
 const handleCanvas = () => {
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  const ctx = canvas.getContext("2d", { willReadFrequently: true });
-  console.log(canvas, ctx);
+  canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  ctx = canvas.getContext("2d", {
+    willReadFrequently: true,
+  }) as CanvasRenderingContext2D;
+  // console.log(ctx);
+  setBgColor();
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  setLineWidth();
+  setColor();
+  // 背景颜色填充整个画布
 };
-const mouseleave = () => {};
+// 设置画笔颜色
+const setColor = () => {
+  ctx.strokeStyle = signConfig.value.color;
+};
+// 设置画笔线宽
+const setLineWidth = () => {
+  ctx.lineWidth = signConfig.value.lineWidth;
+};
+// 设置背景颜色
+const setBgColor = () => {
+  ctx.fillStyle = signConfig.value.fillStyle;
+};
+const reset = () => {
+  signConfig.value = {
+    ...signConfig.value,
+    fillStyle: "#fff", //画布背景颜色
+    color: "#000", //画笔颜色
+    lineWidth: 4, //画笔粗细
+  };
+  setBgColor();
+  setLineWidth();
+  setColor();
+};
+// 鼠标移出
+const mouseleave = () => {
+  isDown = false;
+};
+// 鼠标松开
+const mouseup = () => {
+  //绘制前记录当前的canvas的x/y坐标，画布的宽高
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  // console.log(imgData, "test");
+  //存储所有数据
+  imgDataArr.push(imgData);
+  isDown = false;
+};
+// 上一步
+const preStep = () => {
+  if (!imgDataArr.length) return;
+  if (imgDataArr.length > 1) {
+    const img = imgDataArr.at(-2) as ImageData;
+    imgDataArr.pop(); //移除最后一条数据
+    // console.log(img);
+    ctx.putImageData(img, 0, 0); //填充画布数据
+  } else if (imgDataArr.length === 1) {
+    imgDataArr.pop(); //移除最后一条数据
+    // 最后一步直接清掉画布
+    clearCanvas();
+  }
+};
+// 清空画布
+function clearCanvas() {
+  imgDataArr = [];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+// 保存图片
+const save = () => {
+  const a = document.createElement("a");
+  //图片加载完，再draw 和 toDataURL
+  const dataUrl = canvas.toDataURL();
+  a.href = dataUrl;
+  // 随机字符串名称
+  a.download = getRondomStr();
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
+// 随机字符串
+const getRondomStr = (allLen = 36, endLen = -6) => {
+  //Math.random() 生成随机数字, eg: 0.123456
+  //.toString(36) 转化成36进制 : "0.4fzyo82mvyr"
+  //.slice(-6); 截取最后八位 : "y2mvyr"
+  return Math.random().toString(allLen).slice(endLen);
+};
+// 鼠标按下事件
 const mousedown = (e: MouseEvent) => {
-  console.log(e);
+  // console.log(e, "按下事件");
+  ctx.beginPath();
+  // 设置绘制的起点为当前点击的位置
+  ctx.moveTo(e.offsetX, e.offsetY);
+  // 按下状态修改为true
+  isDown = true;
 };
+// 鼠标移动事件
 const mousemove = (e: MouseEvent) => {
-  console.log(e);
+  // console.log(e);
+  if (isDown) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    // 描边路径
+    ctx.stroke();
+  }
+};
+const changeBgColor = () => {
+  setBgColor();
 };
 onMounted(() => {
   handleCanvas();
