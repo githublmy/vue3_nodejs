ddfa017b56a32f3af547060ee43d262995eb12b6
lmy <455886774@qq.com>
2024-03-09 09:16:44 +0800

    上传

--- lu-vue3-ms/env.d.ts
+++ lu-vue3-ms/env.d.ts
@@ -2,9 +2,10 @@
 // 加密解密
 declare module "jsencrypt/bin/jsencrypt.min";
 
-// 语言包
+// 所有语言包类型
 declare module "*.mjs";
 // declare module "element-plus/dist/locale/zh-cn.mjs"
+
 // 引入sql语言类型
 declare module "monaco-editor/esm/vs/basic-languages/sql/sql.js";
 declare module "monaco-editor/esm/vs/platform/actions/common/actions";
