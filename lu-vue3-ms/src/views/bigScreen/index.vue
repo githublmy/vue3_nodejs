db601e97388aceae63ac57e47e4c45999991062f
lmy <455886774@qq.com>
2024-04-11 23:54:49 +0800

    修改配置

--- lu-vue3-ms/src/views/bigScreen/index.vue
+++ lu-vue3-ms/src/views/bigScreen/index.vue
@@ -93,39 +93,7 @@ const config3 = ref({
 const config = ref({
   value: 66,
 });
-const config2 = ref({
-  data: [
-    {
-      name: "周口",
-      value: 55,
-    },
-    {
-      name: "南阳",
-      value: 120,
-    },
-    {
-      name: "西峡",
-      value: 78,
-    },
-    {
-      name: "驻马店",
-      value: 66,
-    },
-    {
-      name: "新乡",
-      value: 80,
-    },
-    {
-      name: "信阳",
-      value: 45,
-    },
-    {
-      name: "漯河",
-      value: 29,
-    },
-  ],
-  unit: "万元",
-});
+
 </script>
 <style lang="scss" scoped>
 .bigScreen {
